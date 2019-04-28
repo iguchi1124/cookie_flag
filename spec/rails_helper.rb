b4b6f6cb46217462d61f077e37899be40a035c52
Shota Iguchi <shota-iguchi@cookpad.com>
2019-04-29 04:23:30 +0900

    Specify capybara test application server

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,6 +29,8 @@ end
 
 Capybara.default_driver = Capybara.javascript_driver = :selenium_chrome_headless
 
+Capybara.server = :webrick
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
